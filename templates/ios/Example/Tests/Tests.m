655d6374ed9826f92cffbea1bcdf4f00ebb1fa18
lihailong <lihailong@zhizhangyi.com>
2020-10-12 20:27:00 +0800

    完善单元测试模板

--- templates/ios/Example/Tests/Tests.m
+++ templates/ios/Example/Tests/Tests.m
@@ -6,4 +6,34 @@
 //  Copyright (c) TODAYS_YEAR PROJECT_OWNER. All rights reserved.
 //
 
-${TEST_EXAMPLE}
+
+#import <XCTest/XCTest.h>
+
+@interface UUTestUnit : XCTestCase
+
+@end
+
+@implementation UUTestUnit
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
+
